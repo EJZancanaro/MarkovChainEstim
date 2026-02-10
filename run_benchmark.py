a318b0ec7541894f073d476da5c314ad6c5a7aa5
EJZancanaro <eduzancagarcia@hotmail.com>
2026-02-10 16:40:24 +0100

    benchmarking can now be done from the command line

---
+++ run_benchmark.py
@@ -0,0 +1,106 @@
+import argparse
+import numpy as np
+
+from benchmark_from_AFICS import benchmark_from_AFICS
+from benchmark_given_matrix import benchmark_given_matrix
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Benchmark confidence interval methods for Markov chains"
+    )
+
+    # General parameters
+    parser.add_argument("--size_smallest_subsample", type=int, default=1)
+    parser.add_argument("--step_of_subsampling", type=int, default=1)
+    parser.add_argument("--adress_results", required=True ,type=str)
+
+    parser.add_argument(
+        "--methods",
+        nargs="+",
+        default=['Gaussian', 'GaussianSlutsky', 'BasicChi2',
+                 'BasicSlutskyChi2', 'FreerChi2', 'FreerSlutskyChi2'],
+        help="List of CI methods"
+    )
+
+    # Mode selection
+    parser.add_argument(
+        "--mode",
+        choices=["matrix", "afics"],
+        required=True,
+        help="Choose benchmark mode"
+    )
+
+    # ===== Matrix mode arguments =====
+    parser.add_argument("--matrix_id", type=int,
+                        help="Choose example matrix (1-4) among those presented in the article")
+    parser.add_argument("--n_samples", type=int, default=10000)
+    parser.add_argument("--initial_state", type=str, default="A")
+
+    # ===== AFICS mode arguments =====
+    parser.add_argument("--rmsd_file", type=str)
+
+    return parser.parse_args()
+
+
+def load_matrix(matrix_id):
+    if matrix_id == 1:
+        return np.array([
+            [1/3, 1/2, 1/6],
+            [1/3, 1/3, 1/3],
+            [1/2, 1/4, 1/4]
+        ])
+    elif matrix_id == 2:
+        return np.array([
+            [1/3, 1/2, 1/6],
+            [1/2, 0, 1/2],
+            [0, 1, 0]
+        ])
+    elif matrix_id == 3:
+        return np.array([
+            [1/3, 1/3, 1/3],
+            [1/100, 99/100, 0],
+            [1/100, 0, 99/100]
+        ])
+    elif matrix_id == 4:
+        return np.array([
+            [1/1000, 999/1000, 0],
+            [0, 1/1000, 999/1000],
+            [999/1000, 0, 1/1000]
+        ])
+    else:
+        raise ValueError("matrix_id must be between 1 and 4")
+
+
+def main():
+    args = parse_args()
+
+    if args.mode == "matrix":
+        np_true_matrix = load_matrix(args.matrix_id)
+
+        benchmark_given_matrix(
+            np_true_matrix=np_true_matrix,
+            state_space=args.state_space,
+            initial_state=args.initial_state,
+            adress_results=args.adress_results,
+            size_smallest_subample=args.size_smallest_subsample,
+            size_largest_subsample=args.n_samples,
+            step_of_subsampling=args.step_of_subsampling,
+            n_samples=args.n_samples,
+            LIST_METHODS=args.methods
+        )
+
+    elif args.mode == "afics":
+
+        benchmark_from_AFICS(
+            rmsd_file=args.rmsd_file,
+            size_smallest_subsample=args.size_smallest_subsample,
+            step_of_subsampling=args.step_of_subsampling,
+            size_largest_subsample=np.inf,
+            adress_results=args.adress_results,
+            LIST_METHODS=args.methods
+        )
+
+
+if __name__ == "__main__":
+    main()
